d4e7e820da478d26b84c59b4718ae9f26fcf6563
bke243 <48906220+bke243@users.noreply.github.com>
2022-02-18 11:09:18 +0100

    adding room join and leave

--- backend/src/utils/socket.ts
+++ backend/src/utils/socket.ts
@@ -1,6 +1,15 @@
 import { Server, Socket } from "socket.io";
+import RoomService from "../services/RoomService";
+import { formatMessage } from "./room.util";
+
+interface RoomMemebrShip {
+  username: string; 
+  room: string;
+}
 
 let io: any;
+
+const appName = "Chat room";
 
 module.exports = {
   init: (httpServer: Server) => {
@@ -11,10 +20,57 @@ module.exports = {
       }
     });
 
-    io.on("connection", (_socket: any) => {
-        // TODO add more here 
-        console.log(" new Client connected");
+    io.on("connection", (socket: any) => {
+
+      let joinedRoom: string | null = null;
+
+      socket.on('joinRoom', async ({ username, room }: RoomMemebrShip) => {
+        const user = await RoomService.addMemberToRoom(socket.id, username, room);
+
+        // TODO No room with name
+        if (!user) return;
+        
+        joinedRoom = room;
+    
+        socket.join(room);
+    
+        socket.emit('message', formatMessage(appName, 'Welcome to ChatCord!'));
+    
+        socket.broadcast
+          .to(room)
+          .emit(
+            'message',
+            formatMessage(appName, `${username} has joined the chat`),
+          );
+    
+        const roomMembers = await RoomService.getRoomMembers(room);
+        io.to(room).emit('roomUsers', {
+          room: room,
+          users: roomMembers,
+        });
       });
+
+      // TODO send messages in a room
+
+      socket.on('disconnect', async () => {
+        const user = await RoomService.removeMemberFromRoom(socket.id, joinedRoom);
+   
+        if (user && joinedRoom) {
+          const messangeContent = `${user.name} has left the chat`;
+          io.to(joinedRoom).emit(
+            'message',
+            formatMessage(appName, messangeContent),
+          );
+    
+          const roomMembers = await RoomService.getRoomMembers(joinedRoom);
+          io.to(joinedRoom).emit('roomUsers', {
+            room: joinedRoom,
+            users: roomMembers,
+          });
+        }
+      });
+    });
+
     return io as Socket;
   },
   getIO : () => {
